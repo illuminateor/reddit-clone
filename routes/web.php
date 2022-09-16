d4d9a97d335b5707a1ea632527f456ed36c0f6d1
illuminateor <o.mudrak93@gmail.com>
2022-09-16 13:43:28 +0200

    Create community

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 
-    Route::resource('/communties', CommunityController::class);
+    Route::resource('/communities', CommunityController::class);
 });
 
 require __DIR__ . '/auth.php';
